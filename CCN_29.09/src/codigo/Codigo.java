cf689b2f704da65607e812b60486020dab7f9e59
Rudsom <rudsomlima@gmail.com>
2016-09-29 14:42:23 -0300

    Captura boletos CCN

---
+++ CCN_29.09/src/codigo/Codigo.java
@@ -0,0 +1,143 @@
+package codigo;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;  
+
+public class Codigo {
+
+	
+	//public static final String[] Ret_Busca = null;
+	//pega o endereco diretorio documents do usuario
+	private static String raiz_url = "http://www.sysca.com.br/ver_boleto.php?cod=";
+	private static String pathProp = System.getProperty("user.home") + "\\Documents" + "\\BoletoCCN.properties";
+	private static String myDocumentPath = System.getProperty("user.home") + "\\Documents" + "\\BoletoCCN.html";
+    private static int n_page = 52000;
+	public static String Str_n_page = null;
+	//Grava arquivo de configuracao prop
+	static Properties prop = new Properties();
+	static File fProp = new File(pathProp);
+	private static FileWriter arquivo;
+	private static PrintWriter grava_arq;
+	private static String s_nome;
+	public static String[] Ret_busca = {"","",""};
+	private static int n = 0;
+	private static boolean FLAG_LOOP = false;
+	
+	public static boolean isFLAG_LOOP() {
+		return FLAG_LOOP;
+	}
+
+
+	public static void setFLAG_LOOP(boolean fLAG_LOOP) {
+		FLAG_LOOP = fLAG_LOOP;
+	}
+
+
+	///// M�TODOS ////////////////////
+	public static void gravaArquivo() throws IOException{
+		arquivo = new FileWriter(myDocumentPath);
+		grava_arq = new PrintWriter(arquivo);
+	}
+	
+	
+	public static void criaArqProp() throws IOException {				
+		//cria o arquivo se ele nao existir
+		if(!fProp.exists())	{
+			fProp.createNewFile(); 
+			gravaProp();
+			}
+		}
+	
+	//Grava arquivo properties
+	public static void gravaProp() throws IOException {
+		prop.setProperty("Str_n_page", Integer.toString(n_page));
+		FileOutputStream Oprop = new FileOutputStream(pathProp); //instancia de gravacao
+		prop.store(Oprop, null);
+	}
+	
+	public static void carregaArquivo_N_page() throws IOException {
+		FileInputStream Iprop = new FileInputStream(pathProp); //instancia de leitura
+		prop.load(Iprop); //carrega o arquivo
+		n_page = Integer.parseInt(prop.getProperty("Str_n_page"));  //pega o numero da pagina
+		Iprop.close();	
+	}
+		
+	public static void inicioHtml() throws IOException {//Grava arquivo html
+		gravaArquivo();
+		grava_arq.println("<html> <head> </head> <body>");
+		}
+	
+	public static void fimHtml() throws IOException {
+		//gravaArquivo();
+		grava_arq.println("<h1>FIM DE BOLETOS</h1>");
+		grava_arq.println("</p>ULTIMO INDICE ACESSADO: " + n_page);
+		grava_arq.println("<body> <html>");
+		grava_arq.close();				
+	}
+	
+//		//n_page = Integer.parseInt(JOptionPane.showInputDialog("�ltimo �ndice"));
+//		String nome_busca = tela.gets_nome();
+//		//String nome_busca = JOptionPane.showInputDialog("Digite o nome"); 
+//		nome_busca = nome_busca.toUpperCase();
+//		//System.out.print(nome_busca);
+
+    	//Busca nomes - Entra com o nome a ser buscado e retorna um array com NOME, VALIDADE e N_page
+	public static String[] getNomes(String nome_busca, String Str_n_page) throws IOException {		
+		if(Str_n_page!=null && isFLAG_LOOP()) {  //se não vier nada no campo de pagina e eh a primeira execucao, pega o q estiver no arq properties
+			carregaArquivo_N_page();  //puxa do arquivo já em inteiro
+			setFLAG_LOOP(false);
+		}
+		//n_page = Integer.parseInt(Str_n_page);
+		Document doc = Jsoup.connect(raiz_url+n_page).get();
+		doc.baseUri();
+		Elements fim_page = doc.select("body > h2");
+
+		if(fim_page.toString().indexOf("BOLETO")>0) {    //se achar a palavra boleto retorna FIM
+			Ret_busca[0] = "FIM";
+			Ret_busca[1] = "FIM";
+		}
+		else {
+			//setFLAG_LOOP(true);
+			n++;					
+			Elements nome = doc.select("#boleto > table:nth-child(16) > tbody > tr.campos > td > p:nth-child(1)");
+			Elements vencimento = doc.select("#boleto > table:nth-child(11) > tbody > tr.campos > td.vencimento2");
+			s_nome = nome.html();		
+			s_nome = s_nome.substring(s_nome.indexOf("-")+2, s_nome.length());
+			s_nome = Integer.toString(n) + " - " + s_nome;
+			System.out.print(nome.html()+" - ");				
+			System.out.println(vencimento.html());	
+			n_page++;
+			Ret_busca[0] = s_nome;
+			Ret_busca[1] = vencimento.html();
+			Ret_busca[2] = Integer.toString(n_page);
+		}
+		return Ret_busca; //retorna a busca (nome, vencimento)
+	}
+
+	//Grava consulta em HTML
+	public static void consulta_to_Html() throws IOException{
+		//gravaArquivo();
+		String url = raiz_url + Integer.toString(n_page);
+		grava_arq.printf("<a href=\"" + url +"\"" + "><br />" + Ret_busca[0] + "</a>");
+		//<a href="url">link text</a>
+	}
+		
+
+	public static void abreHtml() throws IOException {
+		//JOptionPane.showMessageDialog(null, "Concluido! Abra o arquivo.");
+		Desktop.getDesktop().open(new File (myDocumentPath));
+		System.exit(0);
+		//Runtime.getRuntime().load("f:\\CCN.html");
+	}
+
+}
---
+++ CCN_29.09/src/telas/Tela.java
@@ -0,0 +1,240 @@
+package telas;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ContainerAdapter;
+import java.awt.event.ContainerEvent;
+import java.io.IOException;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+
+import codigo.Codigo;
+
+public class Tela extends JFrame {
+
+	protected static String[] Ret_busca = {null,null};
+
+	public static void main(String[] args) {		
+
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tela frame = new Tela();		
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	private JTextField textField_Nome;
+	private JTextField textField_Venc;
+	private JTextField textField_Str_n_page;
+	private JTextField textField_Result_Nome;
+	protected String[] retb;
+	protected boolean FLAG_BUSCA = true;	
+
+	public Tela() {
+		addContainerListener(new ContainerAdapter() {
+			@Override
+			public void componentAdded(ContainerEvent arg0) {
+				try {
+					Codigo.criaArqProp();
+					textField_Str_n_page.setText(Codigo.Str_n_page);
+					textField_Str_n_page.setText("teste");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}			
+			}
+		});
+		setTitle("Busca boleto CCN - By Rudsom");
+		setResizable(false);
+
+		setBounds(100, 100, 650, 254);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		textField_Nome = new JTextField();
+		textField_Nome.setColumns(10);
+
+		textField_Venc = new JTextField();
+		textField_Venc.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_Venc.setColumns(10);
+
+		textField_Str_n_page = new JTextField();
+		textField_Str_n_page.setForeground(Color.RED);
+		textField_Str_n_page.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_Str_n_page.setColumns(10);
+
+		textField_Result_Nome = new JTextField();
+		textField_Result_Nome.setColumns(10);
+
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//Busca o nome
+				Thread th = new Thread() {
+					public void run() {
+						try {
+							//Codigo.criaArqProp();
+							Codigo.inicioHtml();
+							FLAG_BUSCA = true; //habilita a busca
+							boolean FIM = false;							
+							while(!FIM && FLAG_BUSCA) {  //enquanto fim nao for verdadeiro e não tiver apertado o btn parar busca
+								Ret_busca = Codigo.getNomes(textField_Nome.getText(),textField_Str_n_page.getText());	
+								if(Ret_busca[0]=="FIM") FIM=true;
+								//mostra os nomes achados
+								textField_Result_Nome.setText(Ret_busca[0]);
+								textField_Venc.setText(Ret_busca[1]);
+								textField_Str_n_page.setText(Ret_busca[2]);
+								//grava os arquivos so com os nomes buscados
+								Ret_busca[0] = Ret_busca[0].toUpperCase();
+								//if(Ret_busca[0].indexOf(textField_Nome.getText())>0 || Ret_busca[0]=="" ) { //verifica se retornou o nome que tem no campo da busca	
+									Codigo.consulta_to_Html();
+								//}	
+							}
+							Codigo.fimHtml();	
+							Codigo.gravaProp();
+							Codigo.abreHtml();
+
+					}catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+
+			};
+			th.start();
+
+		}
+	});
+
+
+
+		JLabel lblNome = new JLabel("Nome a buscar:");
+
+		JLabel lblVencimento = new JLabel("Vencimento:");
+
+		JLabel lblNomes = new JLabel("Resultados:");
+
+		JLabel lblIndice = new JLabel("Indice:");
+
+		JButton btnSair = new JButton("Sair");
+		btnSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+
+		JButton btnParar = new JButton("Parar");
+		btnParar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				FLAG_BUSCA = false;
+				try {                        //
+					Codigo.fimHtml();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} //
+			}
+		});
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addGap(26)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(textField_Nome, GroupLayout.PREFERRED_SIZE, 421, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblNome))
+						.addGap(33)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(textField_Str_n_page, GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+								.addComponent(textField_Venc, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE))
+						.addContainerGap(54, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+						.addGap(480)
+						.addComponent(lblVencimento, GroupLayout.DEFAULT_SIZE, 110, Short.MAX_VALUE)
+						.addContainerGap(54, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+						.addGap(480)
+						.addComponent(lblIndice, GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+						.addContainerGap(64, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+						.addGap(26)
+						.addComponent(lblNomes, GroupLayout.PREFERRED_SIZE, 89, GroupLayout.PREFERRED_SIZE)
+						.addGap(529))
+				.addGroup(groupLayout.createSequentialGroup()
+						.addGap(26)
+						.addComponent(textField_Result_Nome, GroupLayout.PREFERRED_SIZE, 421, GroupLayout.PREFERRED_SIZE)
+						.addGap(197))
+				.addGroup(groupLayout.createSequentialGroup()
+						.addGap(26)
+						.addComponent(btnBuscar)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(btnParar)
+						.addPreferredGap(ComponentPlacement.RELATED)
+						.addComponent(btnSair)
+						.addContainerGap(411, Short.MAX_VALUE))
+				);
+		groupLayout.setVerticalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addGap(25)
+						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addGroup(groupLayout.createSequentialGroup()
+										.addComponent(lblNome)
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addComponent(textField_Nome, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addGroup(groupLayout.createSequentialGroup()
+										.addComponent(lblIndice)
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addComponent(textField_Str_n_page, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+						.addGap(41)
+						.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addGroup(groupLayout.createSequentialGroup()
+										.addComponent(lblNomes)
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addComponent(textField_Result_Nome, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+								.addGroup(groupLayout.createSequentialGroup()
+										.addComponent(lblVencimento)
+										.addPreferredGap(ComponentPlacement.RELATED)
+										.addComponent(textField_Venc, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)))
+						.addGap(29)
+						.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE, false)
+								.addComponent(btnBuscar)
+								.addComponent(btnParar)
+								.addComponent(btnSair))
+						.addContainerGap(27, Short.MAX_VALUE))
+				);
+		groupLayout.linkSize(SwingConstants.HORIZONTAL, new Component[] {btnBuscar, btnSair, btnParar});
+		getContentPane().setLayout(groupLayout);
+
+}
+
+public String getIndice() {
+	return textField_Str_n_page.getText();
+}
+
+public String gets_nome() {
+	return textField_Nome.getText();
+}
+
+public void sets_nome(String s_nome) {
+	textField_Nome.setText(s_nome);
+}
+
+public void setVencimento(String html) {
+	textField_Venc.setText(html);
+
+}
+}
